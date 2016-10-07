a7dfefa2d376ea7efff2b31b12eec481d2a24da2
Arnaud Auroux <arnaud@auroux.me>
2016-10-07 16:28:24 +0200

    Add service collection extensions

---
+++ src/GeekLearning.Authorizations.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace GeekLearning.Authorizations.EntityFrameworkCore
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddACL<TContext>(this IServiceCollection services)
+            where TContext : DbContext 
+        {
+            services.AddTransient<IAuthorizationsClient, AuthorizationsClient<TContext>>();
+            services.AddTransient<IAuthorizationsProvisioningClient, AuthorizationsProvisioningClient<TContext>>();
+
+            return services;
+        }
+    }
+}
