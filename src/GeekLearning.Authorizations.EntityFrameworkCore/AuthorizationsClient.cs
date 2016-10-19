fdcfc92abb4a408c6f617feaed24c847740366ec
Arnaud Auroux <arnaud@auroux.me>
2016-10-19 19:02:45 +0200

    Rename sql functions

--- src/GeekLearning.Authorizations.EntityFrameworkCore/AuthorizationsClient.cs
+++ src/GeekLearning.Authorizations.EntityFrameworkCore/AuthorizationsClient.cs
@@ -25,7 +25,7 @@
 
         public async Task<RightsResult> GetRightsAsync(string scopeKey, Guid? principalIdOverride = null, bool withChildren = false)
         {
-            var function = withChildren ? "Authorizations.GetRightsForScopeAndChildren" : "Authorizations.GetInheritedRightsForScope";
+            var function = withChildren ? "Authorizations.GetRightsForScopeAndChildren" : "Authorizations.GetRightsForScope";
             
             using (RelationalDataReader dataReader = await this.context.Database.ExecuteSqlCommandExtAsync(
                                                                                     $"select * from {function}(@scopeName,@principalId)",
