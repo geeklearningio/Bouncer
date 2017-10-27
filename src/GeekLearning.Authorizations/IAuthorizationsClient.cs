29a9b381c024976d06c9540a698849f4a77833e3
Arnaud Auroux <arnaud@auroux.me>
2017-10-27 17:49:49 +0200

    Add DetectMemberships API

--- src/GeekLearning.Authorizations/IAuthorizationsClient.cs
+++ src/GeekLearning.Authorizations/IAuthorizationsClient.cs
@@ -12,14 +12,18 @@
 
         Task<bool> HasExplicitRightOnScopeAsync(string rightName, string scopeName, Guid? principalIdOverride = null);
 
-        Task<IList<Guid>> GetGroupMembersAsync(params string[] groupNames);
+        Task<IList<Guid>> GetGroupMembersAsync(string groupName);
 
-        Task<IList<Guid>> GetGroupMembersAsync(params Guid[] groupIds);
+        Task<IDictionary<string, IList<Guid>>> GetGroupMembersAsync(params string[] groupNames);
 
+        Task<IList<Guid>> GetGroupMembersAsync(Guid groupId);
+        
         Task<IList<Guid>> GetGroupParentLinkAsync(Guid principalId);
 
         Task<bool> HasMembershipAsync(params string[] groupNames);
 
         Task<IList<Guid>> HasMembershipAsync(IEnumerable<Guid> principalIds, params string[] groupNames);
+
+        Task<IList<string>> DetectMembershipsAsync(IEnumerable<string> groupNames);
     }
 }
