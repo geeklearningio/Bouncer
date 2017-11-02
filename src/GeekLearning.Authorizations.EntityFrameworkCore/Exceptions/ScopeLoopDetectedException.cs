2cc213d4b0a2483b5281e1f2ec1217e07093cada
Arnaud Auroux <arnaud@auroux.me>
2017-11-02 16:12:25 +0100

    Fix Scope loop detection

---
+++ src/GeekLearning.Authorizations.EntityFrameworkCore/Exceptions/ScopeLoopDetectedException.cs
@@ -0,0 +1,11 @@
+﻿namespace GeekLearning.Authorizations.EntityFrameworkCore.Exceptions
+{
+    using System;
+
+    public class ScopeLoopDetectedException : Exception
+    {
+        public ScopeLoopDetectedException(string scopeName) : base($"Scope {scopeName} is already part of the scope hierarchy. This indicates a loop in the scope model which is not permitted.")
+        {
+        }
+    }
+}
