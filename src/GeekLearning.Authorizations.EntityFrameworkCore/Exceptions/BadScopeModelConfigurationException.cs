eab4c4aaa10e9754fd1d420fc85bfa8dd01cf28f
Arnaud Auroux <arnaud@auroux.me>
2017-12-04 13:53:08 +0100

    Remove SQL scripts

--- src/GeekLearning.Authorizations.EntityFrameworkCore/Exceptions/BadScopeModelConfigurationException.cs
+++ src/GeekLearning.Authorizations.EntityFrameworkCore/Exceptions/BadScopeModelConfigurationException.cs
@@ -4,11 +4,13 @@
 
     public class BadScopeModelConfigurationException : Exception
     {
-        public BadScopeModelConfigurationException(string scopeParentName, string scopeChildName, int scopeParentlevel, int scopeChildLevel) : base($"Scope {scopeChildName} is a child of scope {scopeParentName} but its hierarchy level is higher or equal ({scopeChildName} vs {scopeParentName}) which indicate a bad scope model configuration")
+        public BadScopeModelConfigurationException(string scopeParentName, string scopeChildName, int scopeParentlevel, int scopeChildLevel)
+            : base($"Scope {scopeChildName} is a child of scope {scopeParentName} but its hierarchy level is higher or equal ({scopeChildLevel} vs {scopeParentlevel}) which indicate a bad scope model configuration")
         {
         }
 
-        public BadScopeModelConfigurationException(string scopeName) : base($"Scope {scopeName} has invalid links which indicate a bad scope model configuration")
+        public BadScopeModelConfigurationException(string scopeName)
+            : base($"Scope {scopeName} has invalid links which indicate a bad scope model configuration")
         {
         }
     }
--- src/GeekLearning.Authorizations.EntityFrameworkCore/Sql/Tables/RoleRight.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [Authorizations].[RoleRight] (
-    [RoleId]  UNIQUEIDENTIFIER NOT NULL,
-    [RightId] UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [PK_RoleRight] PRIMARY KEY CLUSTERED ([RoleId] ASC, [RightId] ASC),
-    CONSTRAINT [FK_RoleRight_Right] FOREIGN KEY ([RightId]) REFERENCES [Authorizations].[Right] ([Id]),
-    CONSTRAINT [FK_RoleRight_Role] FOREIGN KEY ([RoleId]) REFERENCES [Authorizations].[Role] ([Id])
-);
-
